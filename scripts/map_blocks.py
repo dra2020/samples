2744cf960c29f302128ad4b839ae3f2626ea8a69
alecramsay <4155251+alecramsay@users.noreply.github.com>
2021-06-02 06:55:44 -0700

    Setup map_blocks.py script shell

---
+++ scripts/map_blocks.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+#
+# TODO - MAP BLOCKS
+#
+# TODO - For example:
+#
+# map_blocks ...
+#
+# For documentation, type:
+#
+# map_blocks.py -h
+
+# from nagle import *
+
+import sys
+import os
+import argparse
+# import csv
+# from collections import defaultdict
+
+
+# Parse the command line arguments
+
+
+def main():
+    # parser = argparse.ArgumentParser(description='Analyze a plan')
+    # parser.add_argument('vpi_csv', help='VPI by CD .csv')
+    # parser.add_argument('parms_txt', help='Parameters')
+    # parser.add_argument('-v', '--verbose', dest='verbose',
+    #                     action='store_true', help='verbose mode')
+
+    # args = parser.parse_args()
+
+    # vpi_csv = os.path.abspath(args.vpi_csv)
+    # parms_txt = os.path.abspath(args.parms_txt)
+
+    # verbose = args.verbose
+
+    print("TODO - Map blocks ...")
+
+# END
+
+
+# Execute the script
+main()
